3fcdce3ead9f5636f45b9ad8aa5abdf5e75381fe
小鬼难缠 <zjscy666@163.com>
2020-06-09 17:23:50 +0800

    2
    2

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MY SERVERLess
+        </p>
+        <a
+          className="App-link"
+          href="//github.com/zjscy666"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MY GITHUB
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
